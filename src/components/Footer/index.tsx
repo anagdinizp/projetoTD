b1206fc9fb4addf82b10598a8d717048775e37c1
anagdinizp <anagabriela0399@hotmail.com>
2022-09-03 11:33:17 -0300

    Footer em styled components

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,10 @@
 import { LogoFooter } from "../Logo/Logo";
+import { FooterContainer } from "./style";
 
 export function Footer() {
   return (
-    <footer className="flex shrink-0 p-1 w-full justify-center text-center md:relative bottom-0 left-0 md:w-screen bg-white-100 border border-t-gray-200 ">
+    <FooterContainer>
       <LogoFooter />
-    </footer>
-  )
+    </FooterContainer>
+  );
 }
--- src/components/Footer/style.tsx
+++ src/components/Footer/style.tsx
@@ -1,1 +1,20 @@
 import styled from 'styled-components';
+import { point } from "../../components/Breakpoints";
+
+export const FooterContainer = styled.footer`
+  display: flex;
+  flex-shrink: 0;
+  padding: 0.25rem;
+  width: 100%;
+  justify-content: center;
+  text-align: center;
+  @media ${point.tablet} {
+    position: relative;
+    bottom: 0;
+    left: 0;
+    width: 100vw;
+    background-color: white;
+    border-width: 1px;
+    border-top-color: lightgray;
+  }
+`;
