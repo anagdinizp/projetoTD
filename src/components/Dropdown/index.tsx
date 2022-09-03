417a60a9d9783dff2a2624365dca79c5d36a3adc
anagdinizp <anagabriela0399@hotmail.com>
2022-09-03 11:48:44 -0300

    Dropdown em styled components

--- src/components/Dropdown/index.tsx
+++ src/components/Dropdown/index.tsx
@@ -1,24 +1,25 @@
+import {
+  DropdownContainer,
+  LogoOutAnchor,
+  LogoutDropdown,
+  PerfilAnchor,
+  PerfilAnchorContainer,
+  PerfilDropdown,
+  PerfilIcon,
+} from "./style";
+
 export function DropdownUser() {
   return (
-    <div className="invisible md:visible absolute mt-2 top-14 w-36 p-1 overflow-hidden bg-white-100 rounded">
-      <div className="border-b border-b-gray-100">
-         <a
-        href="/perfil"
-        className="text-gray-800 text-sm h-12 flex items-center p-2 transform rounded translate-x-px hover:bg-gray-100"
-      >
-        <img
-          className="m-0"
-          src="https://img.icons8.com/external-anggara-detail-outline-anggara-putra/20/000000/external-user-contact-us-anggara-detail-outline-anggara-putra.png"
-        />
-        <span className="ml-6">Perfil</span>
-      </a>
-      </div>
-      <a
-        href="#"
-        className="text-gray-800 text-sm h-12 flex items-center p-2 transform rounded translate-x-px hover:bg-gray-100"
-      >
-        <span className="ml-12">Sair</span>
-      </a>
-    </div>
+    <DropdownContainer>
+      <PerfilAnchorContainer>
+        <PerfilAnchor href="/perfil">
+          <PerfilIcon src="https://img.icons8.com/external-anggara-detail-outline-anggara-putra/20/000000/external-user-contact-us-anggara-detail-outline-anggara-putra.png" />
+          <PerfilDropdown>Perfil</PerfilDropdown>
+        </PerfilAnchor>
+      </PerfilAnchorContainer>
+      <LogoOutAnchor href="#">
+        <LogoutDropdown>Sair</LogoutDropdown>
+      </LogoOutAnchor>
+    </DropdownContainer>
   );
 }
--- src/components/Dropdown/style.tsx
+++ src/components/Dropdown/style.tsx
@@ -1,1 +1,63 @@
 import styled from 'styled-components';
+import { point } from "../../components/Breakpoints";
+
+export const DropdownContainer = styled.div`
+  visibility: hidden;
+  @media ${point.tablet} {
+    visibility: visible;
+    position: absolute;
+    margin-top: 0.5rem;
+    top: 3.5rem;
+    width: 9rem;
+    pad: 0.25rem;
+    overflow: hidden;
+    background-color: white;
+    border-radius: 0.25rem;
+  }
+`;
+
+export const PerfilAnchorContainer = styled.div`
+  border-bottom-width: 1px;
+  border-bottom-color: lightgray;
+`;
+
+export const PerfilAnchor = styled.a`
+  color: gray;
+  font-size: 0.875rem;
+  line-height: 1.25rem;
+  height: 3rem;
+  display: flex;
+  align-items: center;
+  padding: 0.5rem;
+  transform: translate(var(--tw-translate-x), var(--tw-translate-y)) rotate(var(--tw-rotate)) skewX(var(--tw-skew-x)) skewY(var(--tw-skew-y)) scaleX(var(--tw-scale-x)) scaleY(var(--tw-scale-y));
+  :hover {
+    background-color: lightgray;
+  }
+`;
+
+export const PerfilIcon = styled.img`
+  margin: 0;
+`;
+
+export const PerfilDropdown = styled.span`
+  margin-left: 1.5rem;
+`;
+
+export const LogoOutAnchor = styled.a`
+  color: gray;
+  font-size: 0.875rem;
+  line-height: 1.25rem;
+  height: 3rem;
+  display: flex;
+  align-items: center;
+  pad: 0.5rem;
+  transform: translate(var(--tw-translate-x), var(--tw-translate-y)) rotate(var(--tw-rotate)) skewX(var(--tw-skew-x)) skewY(var(--tw-skew-y)) scaleX(var(--tw-scale-x)) scaleY(var(--tw-scale-y));
+  border-radius: 0.25rem;
+  :hover {
+    background-color: lightgray;
+  }
+`;
+
+export const LogoutDropdown = styled.span`
+  margin-left: 3rem;
+`;
